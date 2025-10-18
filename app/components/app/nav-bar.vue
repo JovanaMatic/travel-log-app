33eae66db6f78d6cfdffea5758814bf2a4d179f9
Jovana Matic <jovana.matic@vast.com>
2025-10-18 21:15:34 +0200

    Fix icon for sign in button in header

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -6,8 +6,9 @@
       </NuxtLink>
     </div>
     <div class="navbar-end">
-      <a class="btn btn-accent">Sign in with gitHub</a>
-      <Icon name="tabler:brand-github" size="24" />
+      <a class="btn btn-accent">Sign in with gitHub
+        <Icon name="tabler:brand-github" size="24" />
+      </a>
     </div>
   </div>
 </template>
