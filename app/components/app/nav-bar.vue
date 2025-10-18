270b468d2a71a32a9a3d851d824f145d51132c02
Jovana Matic <jovana.matic@vast.com>
2025-10-18 21:12:32 +0200

    Add landing page

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel log
+      </NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-accent">Sign in with gitHub</a>
+      <Icon name="tabler:brand-github" size="24" />
+    </div>
+  </div>
+</template>
